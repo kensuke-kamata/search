5c225bba23a330edaf1ecb86fc4d402f5afb7716
kamata_kensuke <kamata_kensuke@cygames.co.jp>
2023-08-22 09:48:27 +0900

    feat: Compare Random and Average

--- test/001/main.cpp
+++ test/001/main.cpp
@@ -9,14 +9,28 @@
 #include "Game.h"
 
 // algo
+#include "Random.h"
 #include "Greedy.h"
+
+#include <iostream>
+#include <random>
 
 int main(int, char**)
 {
-    auto maze = Maze();
-    auto algo = Search::Greedy();
+    std::random_device gen;
+    std::mt19937 eng(gen());
 
-    play(maze, algo);
+    auto algo1 = Search::Random(static_cast<int>(eng()));
+    auto algo2 = Search::Greedy();
+
+    auto score1 = average(algo1, 100, false);
+    auto score2 = average(algo2, 100, false);
+
+    using std::cout;
+    using std::endl;
+
+    cout << "[Random] Average: " << score1 << endl;
+    cout << "[Greedy] Average: " << score2 << endl;
 
     return 0;
 }
