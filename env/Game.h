6b81af0c3c706748630a6ff3e3f05ab935bf7cdd
Ken Kamata <kn12kamata@gmail.com>
2023-08-22 09:04:20 +0900

    feat: Utility for evaluation of algo

--- env/Game.h
+++ env/Game.h
@@ -1,6 +1,46 @@
 #pragma once
 
+#include <iostream>
+#include <random>
+
 #include "Maze.h"
 #include "Search.h"
 
-void play(Maze &maze, Search::IMaze &algo);
+void debug(Maze &maze, bool verbose)
+{
+    using std::cout;
+    using std::endl;
+
+    if (verbose)
+    {
+        cout << maze.ToString() << endl;
+    }
+}
+
+void play(Maze &maze, Search::IMaze &algo, bool verbose = false)
+{
+    debug(maze, verbose);
+
+    while (!maze.IsDone())
+    {
+        auto action = algo.Search(maze);
+        assert(action != ACTION::ILLEGAL);
+        maze.Advance(action);
+
+        debug(maze, verbose);
+    }
+}
+
+double average(Search::IMaze &algo, int n, bool verbose = false)
+{
+    std::random_device gen;
+    std::mt19937 eng(gen());
+    Score score = 0;
+    for (int i = 0; i < n; i++)
+    {
+        auto maze = Maze(eng());
+        play(maze, algo, verbose);
+        score += maze.Evaluate();
+    }
+    return static_cast<double>(score / n);
+}
