e1e2067e23550a13637406031bdca3caad7ee0c4
Ken Kamata <kn12kamata@gmail.com>
2023-08-23 21:54:22 +0900

    feat: Timer

---
+++ env/Timer.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <chrono>
+
+class Timer
+{
+public:
+    // Constructor
+    Timer(const int64_t &thresold)
+    {
+        start_ = std::chrono::high_resolution_clock::now();
+        threshold_ = thresold;
+    }
+
+   bool IsOver() const
+   {
+        auto elapsed = std::chrono::high_resolution_clock::now() - start_;
+        return std::chrono::duration_cast<std::chrono::milliseconds>(elapsed).count() >= threshold_;
+   }
+
+private:
+    std::chrono::high_resolution_clock::time_point start_;
+    int64_t threshold_; // ms
+};
