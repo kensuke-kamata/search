87fc35117c233770701d34c1784c20af5505d04f
Ken Kamata <kn12kamata@gmail.com>
2023-08-24 09:20:07 +0900

    fix: Use END_TURN

--- test/003/main.cpp
+++ test/003/main.cpp
@@ -22,8 +22,8 @@ int main(int, char**)
     // // Width: 1, Depth: 100, Threshold: 10 ms
     auto algo2 = Search::Beams(1, END_TURN, 10);
 
-    auto score1 = average(algo1, 100, 30, 30, 100, false);
-    auto score2 = average(algo2, 100, 30, 30, 100, false);
+    auto score1 = average(algo1, 100, 30, 30, END_TURN, false);
+    auto score2 = average(algo2, 100, 30, 30, END_TURN, false);
 
     using std::cout;
     using std::endl;
